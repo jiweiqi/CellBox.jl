ccea2edc26b8fac22ae258b05d83cd1598f5d602
jiweiqi <jiweiqi10@gmail.com>
2021-03-18 22:24:38 -0400

    fix issues of scatter and move loss plot outside

--- cellbox/callback.jl
+++ cellbox/callback.jl
@@ -5,7 +5,7 @@ cbi = function (p, i_exp)
     gold = predict_neuralode(u0, p_gold, i_exp, nothing, false)
     l_plt = []
     for i in 1:minimum([10, ns])
-        plt = scatter(ts, ode_data[i,:], label="Data");
+        plt = Plots.scatter(ts, ode_data[i,:], label="Data");
         plot!(plt, _ts, pred[i,:], label="Prediction");
         plot!(plt, _ts, gold[i,:], label="Ground truth");
         ylabel!(plt, "x$i")
@@ -25,25 +25,43 @@ cbi! = function (p, i_exp, plt)
     ylabel!(plt, "Protein levels")
     xlabel!(plt, "Time")
     plot!(plt, _ts, gold[1,:], line=:dash, label="Ground truth", color=palette(:tab10)[1]);
-    scatter!(plt, ts, ode_data[1,:], label="Data", color=palette(:tab10)[1]);
+    Plots.scatter!(plt, ts, ode_data[1,:], label="Data", color=palette(:tab10)[1]);
     plot!(plt, _ts, pred[1,:], label="Prediction", color=palette(:tab10)[1]);
     for i in 2:minimum([5, ns])
         plot!(plt, _ts, gold[i,:], line=:dash, label=nothing, color=palette(:tab10)[i]);
-        scatter!(plt, ts, ode_data[i,:], label=nothing, color=palette(:tab10)[i]);
+        Plots.scatter!(plt, ts, ode_data[i,:], label=nothing, color=palette(:tab10)[i]);
         plot!(plt, _ts, pred[i,:], label=nothing, color=palette(:tab10)[i]);
     end
     png(plt, string(fig_path, "/trajectories_cond_", i_exp, ".png"))
 end
 
 cbp = function (p, iter)
-    plt_alpha = scatter(p_gold[:,1],p[:,1])
+    plt_alpha = Plots.scatter(p_gold[:,1],p[:,1])
     xlabel!("ground truth alpha")
     ylabel!("inferred alpha")
-    plt_w = scatter([p_gold[:,2:end]...],[p[:,2:end]...])
+    plt_w = Plots.scatter([p_gold[:,2:end]...],[p[:,2:end]...])
     xlabel!("ground truth w")
     ylabel!("inferred w")
     plt_p = plot([plt_alpha, plt_w]..., legend=false, layout = grid(1, 2), size=(850, 400))
     png(plt_p, string(fig_path, "/p_inference_iter", iter))
+end
+
+cbloss = function ()
+    plt_loss = plot(l_loss_train, yscale=:log10, label="train")
+    plot!(plt_loss, l_loss_val, yscale=:log10, label="val")
+    plot!(plt_loss, l_loss_test, yscale=:log10, label="test")
+    plt_grad = plot(l_grad, yscale=:log10, label="grad_norm")
+    plt_p = plot([l_loss_network[i][1] for i in 1:length(l_loss_network)], label="alpha")
+    plot!(plt_p, [l_loss_network[i][2] for i in 1:length(l_loss_network)], label="w")
+    xlabel!(plt_loss, "Epoch")
+    xlabel!(plt_grad, "Epoch")
+    xlabel!(plt_p, "Epoch")
+    ylabel!(plt_loss, "Loss")
+    ylabel!(plt_grad, "Gradient Norm")
+    ylabel!(plt_p, "Parameter correlation")
+    # ylims!(plt_loss, (-Inf, 1))
+    plt_all = plot([plt_loss, plt_grad, plt_p]..., legend=:top, layout = grid(3, 1), size=(600, 900))
+    png(plt_all, string(fig_path, "/loss_grad"))
 end
 
 l_loss_train = []
@@ -56,12 +74,13 @@ iter = 1
 loss_val_movingavg = Inf
 no_change = 0
 
-cb = function (p, loss_train, loss_val, loss_test, g_norm, loss_p)
+cb = function (p, loss_train, loss_val, loss_test, g_norm)
     global l_loss_train, l_loss_val, l_grad, l_loss_network, iter
     push!(l_loss_train, loss_train)
     push!(l_loss_val, loss_val)
     push!(l_loss_test, loss_test)
-    push!(l_loss_network, loss_p)
+
+    push!(l_loss_network, loss_network(p))
     push!(l_grad, g_norm)
     @save string(ckpt_path, "/mymodel.bson") p opt l_loss_train l_loss_val l_loss_test l_grad l_loss_network iter
 
@@ -74,22 +93,7 @@ cb = function (p, loss_train, loss_val, loss_test, g_norm, loss_p)
             for i_exp in l_exp
                 cbi(p, i_exp)
             end
-
-            plt_loss = plot(l_loss_train, yscale=:log10, label="train")
-            plot!(plt_loss, l_loss_val, yscale=:log10, label="val")
-            plot!(plt_loss, l_loss_test, yscale=:log10, label="test")
-            plt_grad = plot(l_grad, yscale=:log10, label="grad_norm")
-            plt_p = plot([l_loss_network[i][1] for i in 1:length(l_loss_network)], label="alpha")
-            plot!(plt_p, [l_loss_network[i][2] for i in 1:length(l_loss_network)], label="w")
-            xlabel!(plt_loss, "Epoch")
-            xlabel!(plt_grad, "Epoch")
-            xlabel!(plt_p, "Epoch")
-            ylabel!(plt_loss, "Loss")
-            ylabel!(plt_grad, "Gradient Norm")
-            ylabel!(plt_p, "Parameter correlation")
-            # ylims!(plt_loss, (-Inf, 1))
-            plt_all = plot([plt_loss, plt_grad, plt_p]..., legend=:top, layout = grid(3, 1), size=(600, 900))
-            png(plt_all, string(fig_path, "/loss_grad"))
+            cbloss()
             cbp(p, "_tracking")
         end
     end
