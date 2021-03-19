692c9915aec666c97fb9008939161768ba7af819
jiweiqi <jiweiqi10@gmail.com>
2021-03-18 22:25:36 -0400

    see previous one

--- cellbox/header.jl
+++ cellbox/header.jl
@@ -79,3 +79,30 @@ end
 if ispath(ckpt_path) == false
     mkdir(ckpt_path)
 end
+
+if haskey(conf, "grad_max")
+    grad_max = conf["grad_max"]
+else
+    grad_max = Inf
+end
+
+ns = Int64(conf["ns"]); # number of nodes / species
+tfinal = Float64(conf["tfinal"]);
+ntotal = Int64(conf["ntotal"]);  # number of samples for each perturbation
+nplot = Int64(conf["nplot"]);
+batch_size = Int64(conf["batch_size"]);  # STEER
+
+n_exp_train = Int64(conf["n_exp_train"]);
+n_exp_val = Int64(conf["n_exp_val"]);
+n_exp_test = Int64(conf["n_exp_test"]);
+
+n_exp = n_exp_train + n_exp_val + n_exp_test;
+noise = Float64(conf["noise"])
+opt = ADAMW(Float64(conf["lr"]), (0.9, 0.999), Float64(conf["weight_decay"]));
+
+n_iter_max = Int64(conf["n_iter_max"])
+n_plot = Int64(conf["n_plot"]);
+n_iter_buffer = Int64(conf["n_iter_buffer"])
+n_iter_burnin = Int64(conf["n_iter_burnin"])
+n_iter_tol = Int64(conf["n_iter_tol"])
+convergence_tol = Float64(conf["convergence_tol"])
